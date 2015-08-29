3d11bdff19632dee2dc07e75b21012e9e88bd837
ms <schmiddim@gmx.at>
2015-08-29 12:37:11 +0200

    1st commit on branch

--- cli.php
+++ cli.php
@@ -5,6 +5,10 @@
  * Date: 28.08.15
  * Time: 23:34
  * @see this ruby application https://github.com/rmoriz/mvg-live/blob/master/lib/mvg/live.rb
+ * http://www.mvg-live.de/ims/dfiStaticAnzeige.svc?haltestelle=Karl-Theodor-Stra%DFe&ubahn=checked&bus=checked&tram=checked&sbahn=checked
+ *
+ * Select Station
+ * http://www.mvg-live.de/ims/dfiStaticAuswahl.svc?haltestelle=Karl&ubahn=checked&bus=checked&tram=checked&sbahn=checked
  */
 
 use Mvg\Http;
