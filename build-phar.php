b9697ec5e8ed20ba30e9d2d3f1b1cc8a7355fd34
ms <schmiddim@gmx.at>
2015-08-29 20:29:26 +0200

    Building a Phar File

---
+++ build-phar.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: ms
+ * Date: 29.08.15
+ * Time: 20:22
+ * @see https://github.com/secondtruth/php-phar-compiler
+ */
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+use Secondtruth\Compiler\Compiler;
+
+$compiler = new Compiler('./');
+
+$compiler->addIndexFile('cli.php');
+
+
+$compiler->addFile('vendor/autoload.php');
+$compiler->addDirectory('vendor/composer', '!*.php');
+$compiler->addDirectory('vendor/electrolinux', '!*.php');
+$compiler->addDirectory('src', '!*.php');
+
+
+$compiler->compile("build/mvv-cli.phar");
