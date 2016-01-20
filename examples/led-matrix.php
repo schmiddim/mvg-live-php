c936be42d00043f24d3ee19ce70845c1e1ef4c54
ms <schmiddim@gmx.at>
2016-01-20 20:30:39 +0100

    works (hard coded)

---
+++ examples/led-matrix.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+-s '' -e '' -n
+
+*/
+use  Mvg\Parser\Html\Departures;
+use  Mvg\Parser\Html\Stations;
+use Mvg\TextOutput\Departures as TextOutputDepartures;
+use Mvg\TextOutput\Stations as TextOutputStations;
+use Mvg\Factories\Departures as DeparturesFactory;
+use Mvg\RequestHandler\Html\HttpGetDepartures;
+use Mvg\LedMatrixOutput\Departures as LedMatrixOutPutDepartues;
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$searchForStations = array(
+	'Bonner Platz',
+	'Karl-Theodor-Straße'
+);
+$filterForStations = array(
+	'Fürstenried West',
+	'Sendlinger Tor' ,
+	'Einsteinstraße'
+);
+#$filterForStations = [];
+$outputArrays = array();
+$outputArrays['lines'] = array();
+foreach ($searchForStations as $searchForStation) {
+	$searchForStation = trim($searchForStation);
+	$http = new HttpGetDepartures('http', 'www.mvg-live.de', 'ims/dfiStaticAuswahl.svc');
+	$result = $http->getDeparturesForStation($searchForStation);
+	$parser = new Departures($result);
+	$departures = $parser->getDepartures();
+
+
+	$factory = new DeparturesFactory($parser);
+	$outputArrays['lines'][] =( new LedMatrixOutPutDepartues($factory, $filterForStations))->getOutput();
+
+
+}
+$outputArrays['lineCount'] = count($outputArrays['lines']);
+
+echo json_encode($outputArrays);
+$break = true;
